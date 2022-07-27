5ffe5bb4ea71c36736d99cb6dddf84fb8f08f77f
Dylan Moreau <dyl.zm.95@gmail.com>
2022-07-27 18:28:16 -0400

    Chapter 7: Walls & Collisions

--- OverworldMap.js
+++ OverworldMap.js
@@ -1,6 +1,7 @@
 class OverworldMap {
     constructor(config) {
         this.gameObjects = config.gameObjects;
+        this.walls = config.walls || {};
 
         this.lowerImage = new Image();
         this.lowerImage.src = config.lowerSrc;
@@ -14,11 +15,40 @@ class OverworldMap {
             utils.withGrid(10.5) - cameraPerson.x, 
             utils.withGrid(6) - cameraPerson.y )
     }
+
     drawUpperImage(ctx, cameraPerson) {
         ctx.drawImage(this.upperImage,
             utils.withGrid(10.5) - cameraPerson.x, 
             utils.withGrid(6) - cameraPerson.y
         )
+    }
+ 
+    isSpaceTaken(currentX, currentY, direction) {
+        const {x,y} = utils.nextPosition(currentX, currentY, direction)
+        return this.walls[`${x},${y}`] || false;
+    }
+
+    mountObjects() {
+        Object.values(this.gameObjects).forEach(object => {
+
+            //TODO: Determine if should mount
+
+            object.mount(this);
+        })
+    }
+
+    addWall(x, y) {
+        this.walls[`${x},${y}`] = true
+    }
+
+    removeWall(x, y) {
+        delete this.walls[`${x},${y}`]
+    }
+
+    moveWall(wasX, wasY, direction) {
+        this.removeWall(wasX, wasY);
+        const {x,y} = utils.nextPosition(wasX, wasY, direction);
+        this.addWall(x, y);
     }
 }
 
@@ -32,12 +62,18 @@ window.OverworldMaps = {
                 y: utils.withGrid(6),
                 isPlayerControlled: true,
             }),
-           npc1: new Person({
+            npc1: new Person({
                 x: utils.withGrid(7),
                 y: utils.withGrid(9),
                 src: 'images/characters/people/npc1.png'
-           })
-        }
+            })
+        },
+        walls: {
+            [utils.asGridCoord(7,6)]: true,
+            [utils.asGridCoord(8,6)]: true,
+            [utils.asGridCoord(7,7)]: true,
+            [utils.asGridCoord(8,7)]: true,
+        },
     },
     Kitchen: {
         lowerSrc: 'images/maps/KitchenLower.png',
--- Person.js
+++ Person.js
@@ -14,32 +14,51 @@ class Person extends GameObject {
     }
 
     update(state) {
-        this.updatePosition();
-        this.updateSprite(state);
+        if  (this.movingProgressRemaining > 0) {
+            this.updatePosition();
+        } else {
+            // Cases for Walking Here
 
-        if (this.isPlayerControlled && this.movingProgressRemaining === 0 && state.arrow) {
-            this.direction = state.arrow;
+            //Case: Keyboard Ready & Arrow Pressed
+            if (this.isPlayerControlled && state.arrow) {
+                this.startBehavior(state, {
+                    type: 'walk',
+                    direction: state.arrow
+                })
+            }
+            this.updateSprite(state);
+        }
+    }
+
+    startBehavior(state, behavior) {
+        // Set charcter direction to behavior 
+        this.direction = behavior.direction;
+
+        if (behavior.type === 'walk') {
+
+            // Stop here if space not free
+            if (state.map.isSpaceTaken(this.x, this.y, this.direction)) {
+                return;
+            }
+
+            //ready to walk
+            state.map.moveWall(this.x, this.y, this.direction);
             this.movingProgressRemaining = 16;
         }
     }
 
     updatePosition() {
-        if (this.movingProgressRemaining > 0) {
-            const [property, change] = this.directionUpdate[this.direction]
-            this[property] += change;
-            this.movingProgressRemaining -= 1;
-        }
+        const [property, change] = this.directionUpdate[this.direction]
+        this[property] += change;
+        this.movingProgressRemaining -= 1;
     }
 
-    updateSprite(state) {
-
-        if (this.isPlayerControlled && this.movingProgressRemaining === 0 && !state.arrow){
-            this.sprite.setAnimation("idle-"+this.direction);
-            return;
-        }
-
-        if( this.movingProgressRemaining > 0) {
+    updateSprite() {
+        if(this.movingProgressRemaining > 0) {
             this.sprite.setAnimation("walk-"+this.direction)
+            return
         }
+
+        this.sprite.setAnimation("idle-"+this.direction);
     }
 }
